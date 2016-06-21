35038529a28f3869eb4a28cc64bae226d0dc15b7
Alain Plattner <alainplattner@gmail.com>
2016-06-21 11:41:45 +0200

    added the update_all file

---
+++ update_all.sh
@@ -0,0 +1,49 @@
+echo
+echo Updating slepian_alpha
+echo ======================
+cd slepian_alpha
+git pull origin master
+cd ..
+
+echo
+echo Updating slepian_bravo
+echo ======================
+cd slepian_bravo
+git pull origin master
+cd ..
+
+echo
+echo Updating slepian_charlie
+echo ======================
+cd slepian_charlie
+git pull origin master
+cd ..
+
+
+echo
+echo Updating slepian_delta
+echo ======================
+cd slepian_delta
+git pull origin master
+cd ..
+
+
+echo
+echo Updating slepian_golf
+echo ======================
+cd slepian_golf
+git pull origin master
+cd ..
+
+
+echo
+echo Updating slepian_hotel
+echo ======================
+cd slepian_hotel
+git pull origin master
+cd ..
+
+
+echo Updating VectorSlepian
+echo ======================
+git pull origin master
