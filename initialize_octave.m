f953a59cf018741bfc0632f8ba1959afc22a7189
Alain Plattner <alainplattner@gmail.com>
2016-06-23 12:47:05 +0200

    At the moment for Octave: Show warning if mapping package functions do not yet exist. Some of them are being developed online as we speak.

--- initialize_octave.m
+++ initialize_octave.m
@@ -40,9 +40,14 @@ initialize
 
 try poly2cw
 catch
-	disp('poly2cw does not yet exist in the mapping or geometry toolbox.')
+	warning('poly2cw does not yet exist in the mapping package.')
 %	disp('This will lead to problems when trying to prepare named regions.')
 %	disp('Hopefully poly2cw will become available soon in the Octave mapping or geometry package.')
 %	disp('Try updating your packages by running:')
 %	disp('pkg update')
+end
+
+try polybool
+catch 
+	warning('polybool does not yet exist in the mapping package.')
 end
