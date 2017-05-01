d71a3d196aa43e58ce1fa6858bb169439d971787
Alain Plattner <alainplattner@gmail.com>
2017-05-01 11:41:22 -0700

    made updating in MAtlab/Octave possible

---
+++ update_all.m
@@ -0,0 +1,59 @@
+function update_all()
+
+disp('Updating slepian_alpha')
+cd('slepian_alpha');
+system('git checkout master');
+system('git pull origin master');
+cd('..');
+
+disp('Updating slepian_bravo');
+cd('slepian_bravo');
+system('git checkout master');
+system('git pull origin master');
+cd('..');
+
+disp('Updating slepian_charlie');
+cd('slepian_charlie');
+system('git checkout master');
+system('git pull origin master');
+cd('..');
+
+disp('Updating slepian_delta');
+cd('slepian_delta');
+system('git checkout master');
+system('git pull origin master');
+cd('..');
+
+disp('Updating slepian_echo');
+cd('slepian_echo');
+system('git checkout master');
+system('git pull origin master');
+cd('..');
+
+disp('Updating slepian_foxtrot');
+cd('slepian_foxtrot');
+system('git checkout master');
+system('git pull origin master');
+cd('..');
+
+disp('Updating slepian_golf');
+cd('slepian_golf');
+system('git checkout master');
+system('git pull origin master');
+cd('..');
+
+disp('Updating slepian_hotel');
+cd('slepian_hotel');
+system('git checkout master');
+system('git pull origin master');
+cd('..');
+
+disp('Updating slepian_juliet');
+cd('slepian_juliet');
+system('git checkout master');
+system('git pull origin master');
+cd('..');
+
+disp('Updating Slepian')
+system('git checkout master');
+system('git pull origin master');
