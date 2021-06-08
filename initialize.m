2812e8710909e425101a508b56c46bf57097e785
Alain Plattner <alainplattner@gmail.com>
2021-06-08 09:56:00 -0500

    made 'initialize.m' more comfortable by setting a 'sleppath' variable

--- initialize.m
+++ initialize.m
@@ -3,20 +3,25 @@ setenv('OSTYPE','linux')
 %setenv('OSTYPE','solaris')
 disp('If you are using a Solaris machine, change the OSTYPE setting in initialize.m')
 
-addpath('./slepian_zero')
-addpath('./slepian_alpha')
-addpath('./slepian_alpha/REGIONS')
-addpath('./slepian_bravo')
-addpath('./slepian_charlie')
-addpath('./slepian_delta')
-addpath('./slepian_echo')
+% To make these functions accessible from another folder,
+% edit the Slepian directory below and copy this file over 
+slepdir = '.';
+
+
+addpath(fullfile(slepdir,'slepian_zero'))
+addpath(fullfile(slepdir,'slepian_alpha'))
+addpath(fullfile(slepdir,'slepian_alpha/REGIONS'))
+addpath(fullfile(slepdir,'slepian_bravo'))
+addpath(fullfile(slepdir,'slepian_charlie'))
+addpath(fullfile(slepdir,'slepian_delta'))
+addpath(fullfile(slepdir,'slepian_echo'))
 % addpath('./slepian_foxtrott')
-addpath('./slepian_golf')
-addpath('./slepian_hotel')
-addpath('./slepian_hotel/MGS')
-addpath('./slepian_juliet')
-addpath('./nonZonalSlepians')
+addpath(fullfile(slepdir,'slepian_golf'))
+addpath(fullfile(slepdir,'slepian_hotel'))
+addpath(fullfile(slepdir,'slepian_hotel/MGS'))
+addpath(fullfile(slepdir,'slepian_juliet'))
+addpath(fullfile(slepdir,'nonZonalSlepians'))
 
 
-addpath('./grdwrite2p')
+addpath(fullfile(slepdir,'grdwrite2p'))
 
